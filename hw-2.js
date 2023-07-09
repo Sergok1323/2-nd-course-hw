70792d9e338d16edaefdfd6171fc112cf0666ebb
Sergok1323 <  >
2023-07-09 23:39:47 +0300

    Выполнил задания по JS с 1 по 8.

---
+++ hw-2.js
@@ -0,0 +1,42 @@
+let a = 10;
+alert (a);
+a = 20;
+alert (a);
+
+let first = 2007;
+alert (first);
+
+let author = ('Brendan Eich');
+alert ( author);
+
+let x=10;
+let y=2;
+alert(x+y);
+alert(x-y);
+alert(x*y);
+alert(x/y);
+
+let result=2**5;
+alert(result);
+
+let a=9;
+let b=2;
+alert(9%2);
+
+
+let num=1;
+num += 5;
+num -=3;
+num *=7;
+num /=3;
+num ++;
+num --;
+alert (num);
+
+
+
+let age = Number(prompt('Сколько вам лет?'));
+alert(age);
+
+
+let user = {name: Ivan, age: 25, isAdmin:}
