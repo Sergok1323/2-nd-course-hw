cc4d9fad1089142ab82eae69ca14416841000c4d
Sergok1323 <  >
2023-07-24 21:52:21 +0300

    Добавил функцию к кнопке "Играть"

--- hw-5_1.js
+++ hw-5_1.js
@@ -1,3 +1,22 @@
+/* Функция для связи с Игра-1 */
+
 function showQuestion() {
     confirm('Вы действительно хотите продолжить запуск игры?');
+}
+
+/* Функция для кнопки "Играть" */
+
+function season() {
+    let n = Number(prompt('Введите номер месяца'));
+    if (Number.isInteger(n) && n == 1 || n == 2 || n == 12 ) {
+        console.log('Зима');
+    } else if (Number.isInteger(n) && n > 2 && n < 6) {
+        console.log('Весна');
+    } else if (Number.isInteger(n) && n > 5 && n < 9) {
+        console.log('Лето');
+    } else if (Number.isInteger(n) && n > 8 && n < 12) {
+        console.log('Осень');
+    } else {
+        console.log('Вы ввели некорректное значение');
+    }         
 }
