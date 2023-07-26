d40211e829a80390f06ce43257805e513df38487
Sergok1323 <  >
2023-07-27 00:23:54 +0300

    Выполнены задания 1,3,4

---
+++ hw-7.js
@@ -0,0 +1,34 @@
+/* Задание 1 */
+
+let string = "AbraKaADAbra";
+ string = string.toUpperCase();
+ alert(`${string}`);
+
+ /* Задание 2 */
+
+
+
+ /* Задание 3 */
+
+ y = Math.floor(32.58884);
+ console.log(y);
+
+ z = Math.ceil(32.58884);
+console.log(z);
+
+ x = Math.round(32.58884);
+ console.log(x);
+
+
+ /* Задание 4 */
+
+ a = Math.max(52, 53, 49, 77, 21, 32);
+ b = Math.min(52, 53, 49, 77, 21, 32);
+ console.log(b, a);
+
+ /* Задание 5 */
+
+ function randomInt(1, 10) {
+    return Math.random() * (max - min) + min;
+ }
+console.log(randomInt());
